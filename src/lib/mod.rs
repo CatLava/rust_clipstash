9cd85da61aabe8a7eb283aa52a9751a671114957
CatLava <evan.schoening@gmail.com>
2022-10-26 20:19:27 -0700

    working server layer for clip models

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -7,4 +7,6 @@ pub use domain::clip::field::ShortCode;
 pub use domain::clip::ClipError;
 pub use domain::time::Time;
 pub use data::DataError;
+pub use domain::Clip;
+pub use service::ServiceError;
 
--- src/lib/service/ask.rs
+++ src/lib/service/ask.rs
@@ -9,6 +9,22 @@ pub struct GetClip {
     pub shortcode: ShortCode,
     pub password: field::Password,
 }
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NewClip {
+    pub content: field::Content,
+    pub title: field::Title,
+    pub expires: field::Expires,
+    pub password: field::Password,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UpdateClip {
+    pub content: field::Content,
+    pub title: field::Title,
+    pub expires: field::Expires,
+    pub password: field::Password,
+    pub shortcode: field::ShortCode,
+}
 
 impl GetClip {
     pub fn from_raw(shortcode: &str) -> Self {
